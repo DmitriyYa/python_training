27fb77e6d43bf96c0aac4e71df1e1282715dbb69
Dmitriy Yakovlev <dmitriy.m.yakovlev@gmail.com>
2019-03-18 19:32:49 +0700

    FIX Задание №2: Создать вспомогательные методы и классы

--- test_add_group.py
+++ test_add_group.py
@@ -33,12 +33,8 @@ class TestAddGroup(unittest.TestCase):
 
     def create_group(self, wd, group):
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.heder)
-        wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
@@ -46,9 +42,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
-        wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
 
