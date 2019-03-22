d74383354df5e39d5575c95e4ae581d84d3f5a5f
Dmitriy Yakovlev <dmitriy.m.yakovlev@gmail.com>
2019-03-22 15:11:36 +0700

    fix Задание №3: Сделать всё то же самое, но для контактов Хорошо, только надо заменить длинные локаторы вида:

--- test_add_user.py
+++ test_add_user.py
@@ -41,40 +41,31 @@ class TestAddUser(unittest.TestCase):
         wd.find_element_by_name("homepage").send_keys(my_user.homepage)
         wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text(my_user.b_day)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[17]").click()
         wd.find_element_by_name("bmonth").click()
         Select(wd.find_element_by_name("bmonth")).select_by_visible_text(my_user.b_month)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[38]").click()
         wd.find_element_by_name("byear").send_keys(my_user.b_year)
         wd.find_element_by_name("aday").click()
         Select(wd.find_element_by_name("aday")).select_by_visible_text(my_user.a_day)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Anniversary:'])[1]/following::option[3]").click()
         wd.find_element_by_name("amonth").click()
         Select(wd.find_element_by_name("amonth")).select_by_visible_text(my_user.a_month)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Anniversary:'])[1]/following::option[35]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
         wd.find_element_by_name("ayear").send_keys(my_user.a_year)
         wd.find_element_by_name("address2").send_keys(my_user.address2)
         wd.find_element_by_name("phone2").send_keys(my_user.phone2)
         wd.find_element_by_name("notes").send_keys(my_user.notes)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
+        wd.find_element_by_name("submit").click()
 
-    def open_group_page(self, driver):
-        driver.find_element_by_link_text("add new").click()
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("add new").click()
 
-    def login(self, driver):
-        driver.find_element_by_name("user").send_keys("ADMIN")
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_id("LoginForm").submit()
+    def login(self, wd):
+        wd.find_element_by_name("user").send_keys("ADMIN")
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_id("LoginForm").submit()
 
-    def open_home_page(self, driver):
-        driver.get("http://localhost/addressbook/edit.php")
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/edit.php")
 
     def tearDown(self):
         self.wd.quit()
