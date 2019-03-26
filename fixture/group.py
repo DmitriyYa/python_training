afd807e96939338ebcf30b9e6f9481ece54f9ec9
Dmitriy Yakovlev <dmitriy.m.yakovlev@gmail.com>
2019-03-26 17:49:27 +0700

    Задание №7: Сделать все тесты создания, удаления, модификации для групп и контактов

--- fixture/group.py
+++ fixture/group.py
@@ -27,3 +27,14 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.name)
+        wd.find_element_by_name("group_footer").send_keys(group.name)
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
