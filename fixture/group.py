3806c5ae19b27be8c34d78cecc1813683b8cc87c
Dmitriy Yakovlev <dmitriy.m.yakovlev@gmail.com>
2019-03-29 15:14:38 +0700

    Вынес заполнение форм пользователей и групп в отдельные методы

--- fixture/group.py
+++ fixture/group.py
@@ -15,9 +15,7 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.heder)
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_form_group(group)
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
@@ -33,8 +31,12 @@ class GroupHelper:
         self.open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").send_keys(group.name)
-        wd.find_element_by_name("group_footer").send_keys(group.name)
+        self.fill_form_group(group)
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
+
+    def fill_form_group(self, group):
+        wd = self.app.wd
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.heder)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
--- fixture/user.py
+++ fixture/user.py
@@ -9,35 +9,7 @@ class UserHelper:
     def create(self, my_user):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").send_keys(my_user.first_name)
-        wd.find_element_by_name("middlename").send_keys(my_user.middle_name)
-        wd.find_element_by_name("lastname").send_keys(my_user.last_name)
-        wd.find_element_by_name("nickname").send_keys(my_user.nick_name)
-        wd.find_element_by_name("title").send_keys(my_user.title)
-        wd.find_element_by_name("company").send_keys(my_user.company)
-        wd.find_element_by_name("home").send_keys(my_user.home)
-        wd.find_element_by_name("mobile").send_keys(my_user.mobile)
-        wd.find_element_by_name("work").send_keys(my_user.work)
-        wd.find_element_by_name("fax").send_keys(my_user.fax)
-        wd.find_element_by_name("email").send_keys(my_user.email)
-        wd.find_element_by_name("email2").send_keys(my_user.email2)
-        wd.find_element_by_name("email3").send_keys(my_user.email3)
-        wd.find_element_by_name("homepage").send_keys(my_user.homepage)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(my_user.b_day)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(my_user.b_month)
-        wd.find_element_by_name("byear").send_keys(my_user.b_year)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(my_user.a_day)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(my_user.a_month)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(my_user.a_year)
-        wd.find_element_by_name("address2").send_keys(my_user.address2)
-        wd.find_element_by_name("phone2").send_keys(my_user.phone2)
-        wd.find_element_by_name("notes").send_keys(my_user.notes)
+        self.fill_form_user(my_user)
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("home").click()
 
@@ -51,6 +23,12 @@ class UserHelper:
     def edit_first_user(self, my_user):
         wd = self.app.wd
         wd.find_element_by_xpath("/html/body/div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a").click()
+        self.fill_form_user(my_user)
+        wd.find_element_by_name("update").click()
+        wd.find_element_by_link_text("home").click()
+
+    def fill_form_user(self, my_user):
+        wd = self.app.wd
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(my_user.first_name)
         wd.find_element_by_name("middlename").clear()
@@ -97,5 +75,3 @@ class UserHelper:
         wd.find_element_by_name("phone2").send_keys(my_user.phone2)
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(my_user.notes)
-        wd.find_element_by_name("update").click()
-        wd.find_element_by_link_text("home").click()
