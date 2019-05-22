e8cbb729eaf59ae19ce97c67532a5e0154ca5ca3
Dmitriy Yakovlev <dmitriy.m.yakovlev@gmail.com>
2019-05-22 18:05:42 +0700

    FIX Задание №29

--- test/test_del_user.py
+++ test/test_del_user.py
@@ -1,25 +1,24 @@
 from model.myuser import MyUser
 import random
-import pytest
 
 
 def test_del_user(app, db, check_ui):
     if len(db.get_user_list()) == 0:
         app.user.create(MyUser(first_name="user"))
 
-    with pytest.allure.step('Get user list'):
+
         old_users = db.get_user_list()
 
-    with pytest.allure.step('Choice random user in user list'):
+
         user = random.choice(old_users)
 
-    with pytest.allure.step('Del user in addressbook'):
+
         app.user.delete_user_by_id(user.id)
 
-    with pytest.allure.step('Get new user list and assert len old user list '):
+
         new_users = db.get_user_list()
         assert len(old_users) - 1 == len(new_users)
-    with pytest.allure.step('Get new user list and assert len old user list '):
+
         old_users.remove(user)
         assert old_users == new_users
 
